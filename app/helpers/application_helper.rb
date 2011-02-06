f5cd65f973bcadeeb3709d207d133dab56427ed6
Isaac Bowen <ikebowen@gmail.com>
2011-02-06 17:56:51 -0600

    cosmetics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
   
   def show_flash
-    [:error, :notice, :warning, :message].collect do |key|
+    flashes = [:error, :notice, :warning, :message].collect do |key|
       content_tag(:div, flash[key], :class => "flash flash_#{key}") unless flash[key].blank?
-    end.join.html_safe
+    end.join
+    "<div id=\"flash\">#{flashes}</div>".html_safe
   end
 
   def current_person
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,5 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+Event.observe(window, 'load', function() {
+  window.setTimeout(function() {
+    new Effect.Fade('flash', {duration: 0.5});
+  }, 2000);
+});
