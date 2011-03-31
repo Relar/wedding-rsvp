8f67700985ef500ce9848165129bd76285eae29c
Anthony Hook <anthony.hook3@gmail.com>
2011-03-30 19:37:01 -0500

    removed backup directory

--- app/controllers/admin/backup/families_controller.rb
+++
@@ -1,6 +0,0 @@
-class Admin::FamiliesController < ApplicationController
-  before_filter :require_user
-
-  active_scaffold :family do |conf|
-  end
-end 
--- app/controllers/admin/backup/meals_controller.rb
+++
@@ -1,6 +0,0 @@
-class Admin::MealsController < ApplicationController
-  before_filter :require_user
-
-  active_scaffold :meal do |conf|
-  end
-end 
