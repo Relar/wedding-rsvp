bdae05902c8c78a78d9562685fe255aaf7a1ae56
Anthony Hook <anthony.hook3@gmail.com>
2011-07-13 23:10:48 -0500

    Correctly display people that aren't attending, separating those from rejected invites, as well as those who haven't RSVPd yet.

--- app/controllers/admin/attendees_controller.rb
+++ app/controllers/admin/attendees_controller.rb
@@ -18,6 +18,12 @@ class Admin::AttendeesController < ApplicationController
   end
 
   def notattending
-    @people = Person.order('last_name').where(:is_attending_reception => false, :is_attending_ceremony => false, :is_guest => false)
+    @notceremony = Family.where('email is not ""').collect(&:people).flatten.select do |person|
+      !person.is_guest? and !person.is_attending_ceremony? and person.is_invited_ceremony?
+    end
+    @notreception = Family.where('email is not ""').collect(&:people).flatten.select do |person|
+      !person.is_guest? and !person.is_attending_reception?
+    end
+    @peopleunsure = Family.where(:email => [nil, '']).collect(&:people).flatten.reject(&:is_guest?)
   end
 end
